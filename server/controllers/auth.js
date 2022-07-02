46fbba8a2251f0da58bb9c6e200af8d0f642bb8e
Mihai Chiciu <mihai.chiciu@jurata.ch>
2022-07-02 19:09:27 +0300

    added checks for login and signup existing users

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,7 @@
 const { connect } = require('getstream');
 const bcrypt = require('bcrypt');
 const StreamChat = require('stream-chat').StreamChat;
-const crypto = require ('crypto');
+const crypto = require('crypto');
 
 require('dotenv').config();
 
@@ -9,55 +9,58 @@ const api_key = process.env.STREAM_API_KEY;
 const api_secret = process.env.STREAM_API_SECRET;
 const app_id = process.env.STREAM_APP_ID;
 
+const signup = async (req, res) => {
+  try {
+    const { fullName, username, password, phoneNumber } = req.body;
 
-const signup = async (req,res) => {
-    try {
-        const {fullName, username, password, phoneNumber} = req.body;
+    const userId = crypto.randomBytes(16).toString('hex');
 
-        const userId = crypto.randomBytes(16).toString('hex');
+    const serverClient = connect(api_key, api_secret, app_id);
 
-        const serverClient = connect(api_key, api_secret, app_id);
+    const clientServer = StreamChat.getInstance(api_key, api_secret);
 
-        const hashedPassword = await bcrypt.hash(password, 10);
+    const usersResp = await clientServer.queryUsers({});
 
-        const token = serverClient.createUserToken(userId);
+    const alreadyRegistered = usersResp.users.find((user) => user.name === username);
 
-        res.status(200).json({token, fullName, username, userId, hashedPassword, phoneNumber});
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: error });
-        
+    if (alreadyRegistered) {
+      res.status(409).json({ message: 'Utilizatorul este deja inregistrat' });
     }
+    const hashedPassword = await bcrypt.hash(password, 10);
 
+    const token = serverClient.createUserToken(userId);
+
+    res.status(200).json({ token, fullName, username, userId, hashedPassword, phoneNumber });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error });
+  }
 };
-const loginAuth = async (req, res) =>{
-    try {
-        const { username, password } = req.body;
+const loginAuth = async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
-        const server = connect(api_key, api_secret, app_id);
+    const server = connect(api_key, api_secret, app_id);
 
-        const clientServer = StreamChat.getInstance(api_key, api_secret);
+    const clientServer = StreamChat.getInstance(api_key, api_secret);
 
-        const {usersTake} = await clientServer.queryUsers({ name: username });
+    const { usersTake } = await clientServer.queryUsers({ name: username });
 
-        if(!usersTake.length) return res.status(400).json({ message: 'Utilizatorul nu a fost gasit!'});
+    if (!usersTake?.length) return res.status(403).json({ message: 'Utilizatorul nu a fost gasit!' });
 
-        const successfulDecrypt = await bcrypt.compare(password, usersTake[0].hashedPassword);
+    const successfulDecrypt = await bcrypt.compare(password, usersTake[0].hashedPassword);
 
-        const tokenSessionID = server.createUserToken(usersTake[0].id);
+    const tokenSessionID = server.createUserToken(usersTake[0].id);
 
-        if(successfulDecrypt){
-            res.status(200).json({tokenSessionID, fullName: usersTake[0].fullName, username, userId: usersTake[0].id})
-        }else{
-            res.status(500).json({ message: 'Parolă greșită!'});
-        }
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: error });
-        
+    if (successfulDecrypt) {
+      res.status(200).json({ tokenSessionID, fullName: usersTake[0].fullName, username, userId: usersTake[0].id });
+    } else {
+      res.status(500).json({ message: 'Parolă greșită!' });
     }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error });
+  }
 };
 
-module.exports={signup, loginAuth}
+module.exports = { signup, loginAuth };
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,10 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-const {signup, loginAuth} = require('../controllers/auth.js');
+const { signup, loginAuth } = require('../controllers/auth.js');
 
 router.post('/signup', signup);
 router.post('/loginAuth', loginAuth);
-
 
 module.exports = router;
